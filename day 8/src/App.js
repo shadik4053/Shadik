3dab275ad46b492ed4c70f5f63e6f39152adf47b
shadik4053 <151598924+shadik4053@users.noreply.github.com>
2023-11-23 16:12:16 +0530

    Add files via upload

---
+++ day 8/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UseEffectDemo from './components/class_exercise';
+
+function App() {
+  return (
+    <>
+      <UseEffectDemo />
+    </>
+  );
+}
+
+export default App;
